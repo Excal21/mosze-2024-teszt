849792f44d2d8a9ef4a14e6599cf73c03a79f34a
Szakál Gyula Richárd <szgyuszi20@gmail.com>
2024-09-05 18:56:04 +0200

    Hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Itt nem hiányzik egy alsó vonal
-    std::cout << '1-100 ertekek duplazasa' //Itt aposztrof helyett idézőjel kell
-    for (int i = 0;) //itt hiányos a ciklusfeltétel
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //itt a ciklusfeltétel hibás
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //Itt hiányzik egy pontosvessző és nem ír ki semmit
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //Itt a vessző helyett pontosvessző kell a for feltételén belül
+    double atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] //Itt hiányzik egy pontosvessző
+        atlag += b[i];
     }
-    atlag /= N_ELEMENTS; //a változó int lesz, viszont átlagnak double vagy float kellene
+    atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b;
     return 0;
 }
